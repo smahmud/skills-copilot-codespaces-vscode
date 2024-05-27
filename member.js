2ca6bd63f81e885567abf6ac904e3a6df873f9b6
Sabri Mahmud <sabri.mahmud@gmail.com>
2024-05-27 01:10:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,57 @@
+function skillMember() {
+    this.member = null;
+    this.memberId = null;
+    this.memberName = null;
+    this.memberSkill = null;
+    this.memberSkillLevel = null;
+    this.memberSkillLevelName = null;
+    this.memberSkillLevelDescription = null;
+    this.memberSkillLevelValue = null;
+    this.memberSkillLevelValueName = null;
+    this.memberSkillLevelValueDescription = null;
+    this.memberSkillLevelValueValue = null;
+    this.memberSkillLevelValueValueName = null;
+    this.memberSkillLevelValueValueDescription = null;
+    this.setMember = function(member) {
+        this.member = member;
+    }
+    this.setMemberId = function(memberId) {
+        this.memberId = memberId;
+    }
+    this.setMemberName = function(memberName) {
+        this.memberName = memberName;
+    }
+    this.setMemberSkill = function(memberSkill) {
+        this.memberSkill = memberSkill;
+    }
+    this.setMemberSkillLevel = function(memberSkillLevel) {
+        this.memberSkillLevel = memberSkillLevel;
+    }
+    this.setMemberSkillLevelName = function(memberSkillLevelName) {
+        this.memberSkillLevelName = memberSkillLevelName;
+    }
+    this.setMemberSkillLevelDescription = function(memberSkillLevelDescription) {
+        this.memberSkillLevelDescription = memberSkillLevelDescription;
+    }
+    this.setMemberSkillLevelValue = function(memberSkillLevelValue) {
+        this.memberSkillLevelValue = memberSkillLevelValue;
+    }
+    this.setMemberSkillLevelValueName = function(memberSkillLevelValueName) {
+        this.memberSkillLevelValueName = memberSkillLevelValueName;
+    }
+    this.setMemberSkillLevelValueDescription = function(memberSkillLevelValueDescription) {
+        this.memberSkillLevelValueDescription = memberSkillLevelValueDescription;
+    }
+    this.setMemberSkillLevelValueValue = function(memberSkillLevelValueValue) {
+        this.memberSkillLevelValueValue = memberSkillLevelValueValue;
+    }
+    this.setMemberSkillLevelValueValueName = function(memberSkillLevelValueValueName) {
+        this.memberSkillLevelValueValueName = memberSkillLevelValueValueName;
+    }
+    this.setMemberSkillLevelValueValueDescription = function(memberSkillLevelValueValueDescription) {
+        this.memberSkillLevelValueValueDescription = memberSkillLevelValueValueDescription;
+    }
+    this.getMember = function() {
+        return this.member;
+    }
+    this.getMemberId
